7ebfe53040fc659aa56359a997fbc3330632d799
mdibfilho <46792163+mdibfilho@users.noreply.github.com>
2025-03-16 14:34:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,34 +12,13 @@ def home():
 def player_details():
     api_url = 'https://api.tacticusgame.com/api/v1/player'
     headers = {
-        'X-API-KEY': os.environ.get('WARHAMMER_API_KEY'),
-        'accept': 'application/json'
+        'accept': 'application/json',
+        'X-API-KEY': os.environ.get('WARHAMMER_API_KEY')
     }
 
     response = requests.get(api_url, headers=headers)
 
     if response.status_code == 200:
-        return jsonify(response.json())
-    else:
-        return jsonify({"error": response.status_code, "details": response.text}), response.status_code
-
-@app.route('/api/player/details')
-def player_details():
-    api_url = 'https://api.tacticusgame.com/api/v1/player'
-    headers = {
-        'X-API-KEY': os.environ.get('WARHAMMER_API_KEY'),
-        'accept': 'application/json'
-    }
-
-    response = requests.get(api_url, headers=headers)
-
-    if response.status_code == 200:
-        
-        player_info = {
-            'Name': response.player.details.name,
-            'Level': response.player.details.name
-        }            
-
         return jsonify(response.json())
     else:
         return jsonify({"error": response.status_code, "details": response.text}), response.status_code
